a05797f620165d8c4a28b2f5db21b7540eade8f4
diaa-woo <dev.dntjr0501@gmail.com>
2024-09-24 17:58:40 +0900

    refactor | change the timer text setting part to reusable

--- timer.py
+++ timer.py
@@ -12,12 +12,25 @@ class Timer(QWidget):
         self.ui = TimerUi()
         self.ui.setup(self)
 
-        self.remaining_time = 1500
+        self.remaining_time = 300
+        self.__timer_text_set()
 
         self.timer = QTimer()
         self.timer.setInterval(1000)
         self.timer.timeout.connect(self.__timecount_cb)
         self.timer.start()
+
+    def __timer_text_set(self):
+        minutes = str(int(self.remaining_time / 60))
+        if len(minutes) < 2:
+            minutes = "0" + minutes
+        seconds = str(self.remaining_time % 60)
+        if len(seconds) < 2:
+            seconds = "0" + seconds
+        self.ui.remain_minutes_ten.setText(minutes[0])
+        self.ui.remain_minutes_one.setText(minutes[1])
+        self.ui.remain_seconds_ten.setText(seconds[0])
+        self.ui.remain_seconds_one.setText(seconds[1])
 
     def __timeout_event(self):
         msg = QMessageBox(self)
@@ -33,16 +46,7 @@ class Timer(QWidget):
             self.__timeout_event()
         else:
             self.remaining_time -= 1
-            minutes = str(int(self.remaining_time / 60))
-            if len(minutes) < 2:
-                minutes = "0" + minutes
-            seconds = str(self.remaining_time % 60)
-            if len(seconds) < 2:
-                seconds = "0" + seconds
-            self.ui.remain_minutes_ten.setText(minutes[0])
-            self.ui.remain_minutes_one.setText(minutes[1])
-            self.ui.remain_seconds_ten.setText(seconds[0])
-            self.ui.remain_seconds_one.setText(seconds[1])
+            self.__timer_text_set()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv) 
